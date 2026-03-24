0064deeda3198c6e95bcacce86f8f86b46d33a24
priyankapillalamarri <priyankapillalamarri9.in>
2026-03-24 23:54:45 +0530

    odd number in a vector simple program

---
+++ vectors/odd_nums.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<int> nums = {1,2,3,4,5,6};
+
+    for(int i=0; i<nums.size(); i++) {
+        if(nums[i]%2!=0) {
+            cout<<nums[i]<<" ";
+        }
+    }
+    return 0;
+}
