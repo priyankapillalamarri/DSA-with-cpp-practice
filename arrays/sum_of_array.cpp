6b7be8cdab28d0eb5f80ad9c7b6d4deeda1f6050
priyankapillalamarri <priyankapillalamarri9.in>
2026-03-25 00:03:04 +0530

    sum of elements in an array program

---
+++ arrays/sum_of_array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {1,2,3,4,5};
+    int n = 5, sum=0;
+
+    for(int i=0; i<5; i++) {
+        sum = sum + arr[i];
+    }
+    cout<<"sum is: "<<sum<<endl;
+    return 0;
+}
