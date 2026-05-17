9314ef9937cc9d59d7f92c077f0e83fbaafe58c8
priyankapillalamarri <priyankapillalamarri9.in>
2026-05-17 15:39:16 +0530

    box shape pattern problem

--- patterns/simple.cpp
+++ patterns/simple.cpp
@@ -7,9 +7,23 @@ int main() {
     cin>>n;
     for(int i=1; i<=n; i++) {
         for(int j=1; j<=n; j++) {
-            cout<<"*";
+            if(i==1) {
+                cout<<"*";
+            }
+            else if(i==n) {
+                cout<<"*";
+            }
+            else if(j==1) {
+                cout<<"*";
+            }
+            else if(j==n) {
+                cout<<"*";
+            }
+            else {
+                cout<<" ";
+            }
         }
-        cout<<endl;        
+        cout<<endl;
     }
     return 0;
 }
