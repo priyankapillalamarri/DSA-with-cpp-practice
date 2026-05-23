cd23f242aa97837044c4d61a2768f43ed7c18345
priyankapillalamarri <priyankapillalamarri9.in>
2026-05-23 20:37:54 +0530

    simple n*n number pattern problem

---
+++ patterns_with_numbers/simple_pattern.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"enter n: ";
+    cin>>n;
+
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n; j++) {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+/*output for n=3
+111
+222
+333
+*/
