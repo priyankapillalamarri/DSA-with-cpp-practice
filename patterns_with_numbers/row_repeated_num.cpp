54c408b227d055c1c140edbf734ac3b8f451115a
priyankapillalamarri <priyankapillalamarri9.in>
2026-05-30 20:33:15 +0530

    traingle pattern row repeated numbers problem

---
+++ patterns_with_numbers/row_repeated_num.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"enter n: ";
+    cin>>n;
+
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=i; j++) {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+/*Output for n=4 
+1
+22
+333
+4444
+*/
