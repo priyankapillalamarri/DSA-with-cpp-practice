c8e2213e582d2586fae2bad838447098e1bedbcd
priyankapillalamarri <priyankapillalamarri9.in>
2026-06-17 23:29:30 +0530

    product of array except itself by brute force

---
+++ arrays/prodof_array_except_itself_brute.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> productExceptSelf(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> ans(n,1);
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(i != j) {
+                ans[i] *= nums[j];
+            }
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {1,2,3,4};
+    vector<int>result = productExceptSelf(nums);
+
+    for(int i=0; i<nums.size(); i++) {
+        cout<<result[i]<<" ";
+    }
+}
