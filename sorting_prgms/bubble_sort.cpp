9c0ccaef0a61592a1728d22c71643f41b3c96c3e
priyankapillalamarri <priyankapillalamarri9.in>
2026-09-18 23:06:51 +0530

    bubble sort program

---
+++ sorting_prgms/bubble_sort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void bubble_sort(int arr[], int n) {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+void print_arr(int arr[], int n) {
+    for(int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main() {
+    int arr[] ={3,2,5,1,4};
+    int n = 5;
+
+    bubble_sort(arr,n);
+    print_arr(arr,n);
+    
+    return 0;
+}
